763e331fbc778ae5ce32e47bf8339d0c74f406c2
Theodor Levi Halldorsson <theodorlevi@protonmail.com>
2025-01-31 09:51:32 +0000

    added stuff ig

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,63 @@
 use std::io::Write;
 use inquire::{InquireError, Select};
-use std::process::Command;
-use inquire::{Confirm, Text, MultiSelect};
+use std::process::{exit, Command};
+//use inquire::{Confirm, Text, MultiSelect};
 
 fn main() {
-    match insital_selection() {
-        1 => orka(),
-        2 => orka_framleidd(),
-        3 => orka_dreift(),
-        4 => orka_nytt(),
-        _ => {}
+    loop {
+        match insital_selection() {
+            1 => orka(),
+            2 => orka_framleidd(),
+            3 => orka_dreift(),
+            4 => orka_nytt(),
+            5 => break,
+            _ => {exit(1)}
+        }
     }
+    exit(0);
 }
 
 fn orka_framleidd() {
     icat("./assets/vatnsvirkun.jpg");
-    println!("Hvernig orka er framleidd á íslandi\n
-    \n
-    ");
+    println!("Hvernig orka er framleidd á íslandi\n");
+    println!("meirihluti orku á íslandi er framleidd í vatnsvirkju stöðvum.");
+    println!("vatnsvirkju stöðvar eru staðsettar um mið punkt áar.");
+    println!("vatnið frá ánni snýr túrbínum sem snúa rafalunum");
     std::io::stdout().flush().unwrap();
+    let selection = create_select(
+        "halda áfram?",
+        &[
+            "Já",
+            "Nei",
+        ]
+    );
+    match selection {
+        Ok(1) => return,
+        Ok(2) => exit(0),
+        _ => {}
+    }
 }
 
 fn orka() {
-    todo!()
+    icat("./assets/atom.jpg");
+    println!("hvað er orka\n");
+    println!("orka er hreyfing á frumeindum.");
+    println!("hreyfingin sínir sig sem hiti. rafmagn er typa af orku");
+    println!("vegna þess að það er hreyfing á rafeindum.");
+    println!("ljós er týpa af orku, það er ekki með neinn massa og hryfist bara.");
+    std::io::stdout().flush().unwrap();
+    let selection = create_select(
+        "halda áfram?",
+        &[
+            "Já",
+            "Nei",
+        ]
+    );
+    match selection {
+        Ok(1) => return,
+        Ok(2) => exit(0),
+        _ => {}
+    }
 }
 
 fn orka_dreift() {
@@ -40,7 +75,8 @@ fn insital_selection() -> usize {
                 "Hvað er orka?",
                 "Hvernig er orka framleidd á íslandi?",
                 "Hvernig er orku dreift?",
-                "Hvernig er orkan nýtt?"
+                "Hvernig er orkan nýtt?",
+                "stopp"
             ]
     ).unwrap();
     return selection;
