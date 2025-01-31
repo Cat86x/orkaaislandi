b99d027db883cbd06cbc789b522700f22edf1339
Theodor Levi Halldorsson <theodorlevi@protonmail.com>
2025-01-31 11:26:56 +0000

    finished

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::process::{exit, Command};
 
 fn main() {
     loop {
+        //miðaðvið hvað var valið keyra það function
         match insital_selection() {
             1 => orka(),
             2 => orka_framleidd(),
@@ -17,57 +18,53 @@ fn main() {
     exit(0);
 }
 
+//hvernig orka er framleidd
 fn orka_framleidd() {
     icat("./assets/vatnsvirkun.jpg");
     println!("Hvernig orka er framleidd á íslandi\n");
     println!("meirihluti orku á íslandi er framleidd í vatnsvirkju stöðvum.");
     println!("vatnsvirkju stöðvar eru staðsettar um mið punkt áar.");
-    println!("vatnið frá ánni snýr túrbínum sem snúa rafalunum");
+    println!("vatnið frá ánni snýr túrbínum sem snúa rafalunum\n");
     std::io::stdout().flush().unwrap();
-    let selection = create_select(
-        "halda áfram?",
-        &[
-            "Já",
-            "Nei",
-        ]
-    );
-    match selection {
-        Ok(1) => return,
-        Ok(2) => exit(0),
-        _ => {}
-    }
+    continue_selection();
 }
 
+//hvað er orka
 fn orka() {
     icat("./assets/atom.jpg");
     println!("hvað er orka\n");
     println!("orka er hreyfing á frumeindum.");
     println!("hreyfingin sínir sig sem hiti. rafmagn er typa af orku");
     println!("vegna þess að það er hreyfing á rafeindum.");
-    println!("ljós er týpa af orku, það er ekki með neinn massa og hryfist bara.");
+    println!("ljós er týpa af orku, það er ekki með neinn massa og hryfist bara.\n");
     std::io::stdout().flush().unwrap();
-    let selection = create_select(
-        "halda áfram?",
-        &[
-            "Já",
-            "Nei",
-        ]
-    );
-    match selection {
-        Ok(1) => return,
-        Ok(2) => exit(0),
-        _ => {}
-    }
+    continue_selection();
 }
 
+//hvernig orku er dreift
 fn orka_dreift() {
-    todo!()
+    icat("assets/powerlines.jpg");
+    println!("hvernig er orku er dreift\n");
+    println!("orku er oftast dreift í formi rafmagns");
+    println!("rafmagið er flutt í gegnum háspennu víra.");
+    println!("vírum er síðan leitt undir jörðin inní hús");
+    println!("orka getur líka verið dreifð í formi eldsneytis eða batteríum\n");
+    std::io::stdout().flush().unwrap();
+    continue_selection();
 }
 
+//hvernig orka er nýtt
 fn orka_nytt() {
-    todo!()
+    icat("./assets/homeapl.jpg");
+    println!("Hvernig orka er nýtt\n");
+    println!("orka er notuð fyrir næstum allt á heimilinu.");
+    println!("orka í formi rafmagns er oftast notuð en t.d.");
+    println!("eru bílar sem nota bensín\n");
+    std::io::stdout().flush().unwrap();
+    continue_selection();
 }
 
+//til að velja hvað þú villt vita um
 fn insital_selection() -> usize {
     let selection = create_select(
         "Orka á íslandi\nVeldu síðu",
@@ -82,6 +79,7 @@ fn insital_selection() -> usize {
     return selection;
 }
 
+//sýna mynd
 fn icat(filename: &str) {
         Command::new("icat")
             .arg(filename)
@@ -89,6 +87,7 @@ fn icat(filename: &str) {
             .unwrap();
 }
 
+//búa til val á lista
 fn create_select(prompt: &str, options: &[&str]) -> Result<usize, InquireError> {
     let selection = Select::new(prompt, options.to_vec())
         .with_help_message("Notaðu örvatakkana til að velja og ENTER til að staðfesta")
@@ -101,4 +100,20 @@ fn create_select(prompt: &str, options: &[&str]) -> Result<usize, InquireError> 
         .unwrap_or(0);
 
     Ok(index)
+}
+
+//hvort þú villt halda áfram eða ekki
+fn continue_selection() {
+    let selection = create_select(
+        "halda áfram?",
+        &[
+            "Já",
+            "Nei",
+        ]
+    );
+    match selection {
+        Ok(1) => return,
+        Ok(2) => exit(0),
+        _ => {}
+    }
 }
